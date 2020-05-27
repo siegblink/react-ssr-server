cf2f105dcba625c45f1300ac77e40642f96192d1
Sieg <siegblink@gmail.com>
2020-05-27 20:48:51 +0800

    Refactor index.js file to make use of matchRoutes from react-router-config.

--- src/client/client.js
+++ src/client/client.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import { renderRoutes } from 'react-router-config'
 import Routes from './Routes'
 import reducers from './reducers'
 
@@ -15,7 +16,7 @@ const store = createStore(reducers, {}, applyMiddleware(thunk))
 ReactDOM.hydrate(
   <Provider store={store}>
     <BrowserRouter>
-      <Routes />
+      <div>{renderRoutes(Routes)}</div>
     </BrowserRouter>
   </Provider>,
   document.querySelector('#root')
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
+import { matchRoutes } from 'react-router-config'
+import Routes from './client/Routes'
 import express from 'express'
 import renderer from './helpers/renderer'
 import createStore from './helpers/createStore'
@@ -11,8 +13,7 @@ app.use(express.static('public'))
 app.get('*', function (req, res) {
   const store = createStore()
 
-  // Some logic to initialize
-  //  and load data into the store
+  console.log(matchRoutes(Routes, req.path))
   
   res.send(renderer(req, store))
 })
