f7cc75c3c87d0273a172235f9e45bbf62dbf43de
Sieg <siegblink@gmail.com>
2020-05-27 19:35:08 +0800

    Revert "Make use of react-router-config library."
    This reverts commit a78f7210de76078e8dcf0c1f4777a16d9ee73a81.

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { renderRoutes } from 'react-router-config'
+import {Provider} from 'react-redux'
 import Routes from '../client/Routes'
 
 export default function (req, store) {
   const content = renderToString(
     <Provider store={store}>
       <StaticRouter location={req.path} context={{}}>
-        <div>{renderRoutes(Routes)}</div>
+        <Routes />
       </StaticRouter>
     </Provider>
   )
